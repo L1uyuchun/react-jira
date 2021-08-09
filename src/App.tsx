33293b20534f85d306dee8028c00551f715219c1
liuyc <2786161648@qq.com>
2021-08-09 14:01:50 +0800

    feat: 初始化项目校验

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,25 @@
-import React from 'react'
-import logo from './logo.svg'
-import './App.css'
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
-  console.log(123)
+  console.log(123);
   return (
     <header className="App-header">
       <img src={logo} className="App-logo" alt="logo" />
       <p>
         Edit <code>src/App.tsx</code> and save to reload.
       </p>
-      <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+      <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
         Learn React
       </a>
     </header>
-  )
+  );
 }
 
-export default App
+export default App;
