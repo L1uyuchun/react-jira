3c843b8b164b74ee75730121698deead135f5463
liuyc <2786161648@qq.com>
2021-08-09 11:56:27 +0800

    ceshi

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,9 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      {" "}
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          {" "}
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
